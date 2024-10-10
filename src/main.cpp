59895ee523e7f22d71da7c253bcfb248665831a3
NicholasLuis <nml5604@psu.edu>
2024-10-09 21:00:06 -0400

    Using matplotlibcpp instead of OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -7,8 +5,7 @@
 #include <array>
 #include "RK4.hpp"
 #include "MatrixMath.hpp"
-
-
+#include "matplotlibcpp.h"
 
 
 /*
@@ -108,41 +105,7 @@ int main()
 	}
 
 	// Plotting Stuff
-	{
-		if (!glfwInit()) {
-			std::cerr << "Failed to initialize GLFW" << std::endl;
-			return -1;
-		}
-		GLFWwindow* window = glfwCreateWindow(1000, 500, "Plotting stuff", NULL, NULL);
-		if (!window) {
-			std::cerr << "Failed to create GLFW window" << std::endl;
-			glfwTerminate();
-			return -1;
-		}
-		glfwMakeContextCurrent(window);
-		if (glewInit() != GLEW_OK) {
-			std::cerr << "Failed to initialize GLEW" << std::endl;
-			return -1;
-		}
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		while (!glfwWindowShouldClose(window)) {
-			glClear(GL_COLOR_BUFFER_BIT);
-			glBegin(GL_LINES);
-			// Draw X and Y axis
-			glColor3f(1.0, 1.0, 1.0);
-			glVertex2f(-1.0, 0.0);
-			glVertex2f(1.0, 0.0);
-			glVertex2f(0.0, 1.0);
-			glVertex2f(0.0, -1.0);
 
-			// Plot stuff here
-
-			glEnd();
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-		}
-		glfwTerminate();
-	}
 
 	return 0;
 }
